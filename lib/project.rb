78497c9e1794f1b6a06766ea10b63f02715fb855
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-01 01:21:44 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,5 +9,5 @@ class Project
   def add_backer(backer) 
     @backers << backer 
     backers.backed_project << self unless backer.backed_project.iclude?(self)
-  end unless project.backers.include?(self
+  end 
 end 
